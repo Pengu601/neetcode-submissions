d9b7d44a438d6f302aba4c4aa182666e5d1129e6
Nicholas Gonzalez <111535906+Pengu601@users.noreply.github.com>
2026-08-08 12:21:38 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    //approach is to use a hashmap where key is sorted string and value is list of original strings
+    //After looping through strs list, go through map and append values of each hash key
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        std::unordered_map<string, vector<string>> sorted;
+        if(strs.size() == 1)
+        {
+            res.push_back(strs);
+            return res;
+        }
+
+        for(int i = 0; i < strs.size(); i++)
+        {
+            string str = strs[i];
+            std::sort(str.begin(), str.end());
+            sorted[str].push_back(strs[i]);
+        }
+
+        for(const auto& [key,value] : sorted)
+        {
+            res.push_back(value);
+        }
+
+        return res;
+    }
+};
